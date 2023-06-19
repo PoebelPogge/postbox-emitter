ff98e1fedd989ededb8c3b84b83fad8f31b53895
PoebelPogge <pogge06@googlemail.com>
2023-06-19 12:03:21 +0200

    rework of WiFi connection

--- src/main.cpp
+++ src/main.cpp
@@ -16,54 +16,75 @@ bool emitted = false;
 
 //function declarations
 void connectWiFi();
+void disconnectWiFi();
 void connectMQTT();
 void publishValue(bool emitted);
 void messageReceived(String &topic, String &payload);
 
+//initial Setup
 void setup() {
   Serial.begin(115200);
   pinMode(PIN, INPUT);
 
-  connectWiFi();
+  Serial.println("Device started!");
+}
+
+
+// programm loop
+void loop() {
+  Serial.println("Measurment ...");
+  emitted = digitalRead(PIN);
+
+  if(emitted){
+    Serial.println("Accident Detected");
+    connectWiFi();
+    connectMQTT();
+    mqttClient.loop();
+    publishValue(emitted);
+    Serial.println("Data Transmitted");
+    disconnectWiFi();
+    emitted = false;
+  }
+  delay(500);
+}
+
+// establishing Wifi Connection
+void connectWiFi(){
+  Serial.print("\nConnecting to WiFi");
+  WiFi.forceSleepWake();
+  delay(1);
+   WiFi.mode(WIFI_STA);
+
+  WiFi.begin(ssid, password); // begin WiFi connection
+    // Wait for connection
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
 
   Serial.println("");
   Serial.print("Connected to ");
   Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-
-  Serial.println("Connecting to MQTT broker...");
-  mqttClient.begin(MQTT_BROKER_ADDR, net);
-  mqttClient.onMessage(messageReceived);
-  mqttClient.setWill("post-box-emitter/status", "offline");
-  connectMQTT();
-  Serial.println("Connection to MQTT brocker established");
-
-  Serial.println("Device started!");
 }
 
-void loop() {
-  mqttClient.loop();
-
-  if(!mqttClient.connected()){
-    Serial.println("Lost connection to MQTT broker, reconnecting...");
-    connectMQTT();
-  }
-  Serial.print("\nMeasurment ...");
-  emitted = digitalRead(PIN);
-
-  if(emitted){
-    Serial.print("\nAccident Detected");
-    publishValue(emitted);
-    Serial.print("\nData Transmitted");
-    emitted = false;
-  }
-  delay(500);
+// disconnecting WiFi Connection
+void disconnectWiFi(){
+  WiFi.mode( WIFI_OFF );
+  WiFi.forceSleepBegin();
+  Serial.println("WiFi is down");
 }
-
 
 //Establish MQTT Connection
 void connectMQTT(){
+  Serial.println("Connecting to MQTT broker...");
+
+  mqttClient.begin(MQTT_BROKER_ADDR, net);
+  // mqttClient.onMessage(messageReceived);
+  mqttClient.setWill("post-box-emitter/status", "offline");
+
   while (!mqttClient.connect("post-box-emitter", MQTT_USER_NAME, MQTT_USER_PW))
   {
     Serial.print(".");
@@ -74,23 +95,12 @@ void connectMQTT(){
   mqttClient.publish("post-box-emitter/localIp", WiFi.localIP().toString());
 
   mqttClient.subscribe("post-box-emitter/emitted");
-}
 
-void connectWiFi(){
-  WiFi.begin(ssid, password); // begin WiFi connection
-    // Wait for connection
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
-  }
+  Serial.println("Connection to MQTT brocker established");
 }
 
 // publish mqtt value to broker
 void publishValue(bool emitted){
-
-  Serial.print("\nCurrent emitter value: ");
-  Serial.print(emitted);
   mqttClient.publish("post-box-emitter/emitted", emitted ? "ON" : "OFF");
 }
 
